749d0d0de04b458c145da53f793ce7a2d47f246b
Rigo Dominguez <rigdomin@cisco.com>
2017-06-23 13:30:13 -0400

    fixed semicolons

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -5,17 +5,17 @@ public class Rectangle
 
 	public Rectangle(int length, int width)
 	{
-		this.length = length
-		this.width  = width
+		this.length = length;
+		this.width  = width;
 	}
 
 	public int getArea()
 	{
-		return this.length * this.width
+		return this.length * this.width;
 	}
 
 	public int getPerimeter()
 	{
-		return 2 * (this.length + this.width)
+		return 2 * (this.length + this.width);
 	}
 }
